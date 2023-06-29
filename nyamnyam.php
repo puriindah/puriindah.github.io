d86c97fa86fdb5318b27eb847604fad2508ca02e
puriindah <138136723+puriindah@users.noreply.github.com>
2023-06-29 22:10:26 +0700

    Update nyamnyam.php

--- nyamnyam.php
+++ nyamnyam.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seperti Yang Dulu Pernah Di Janjikan Sang Mantan/title>
+    <title>Seperti Yang Dulu Pernah Di Janjikan Sang Mantan<title>
     <style>
         body {
             padding: 0;
